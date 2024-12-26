d9865586e1b52cd904e6a05f369b458075a607fc
TsioryRasoloarijaona <hei.tsiory@gmail.com>
2024-12-26 22:09:30 +0100

    refractor : adding comments

--- covid_data_transformation/all_files_transform.R
+++ covid_data_transformation/all_files_transform.R
@@ -1,10 +1,13 @@
 library(pdftools)
 library(tidyverse)
+library(googledrive)
 
 
+#delete all arabic caracter
 remove_arabic_text <- function(text) {
   gsub("[\u0600-\u06FF]+", "", text)
 }
+
 
 remove_numeric_text <- function(text) {
   gsub("[0-9]", "", text)
@@ -13,33 +16,40 @@ remove_numeric_text <- function(text) {
 
 extract_transform <- function(fichier) {
   
+  #extract all text in the file
   pdf_1 <- pdf_text(fichier)
   
+  #estract all date formats among the text (format : **/**/**)
   dates <- str_extract_all(pdf_1, "\\d{2}/\\d{2}/\\d{4}")
   
+  #check if there are extraced dates
   if (length(dates[[1]]) == 0) {
     warning(paste("Aucune date trouvée dans le fichier", fichier))
     return(NULL)
   }
   
+  #reformate the first found date to (dd/mm/yy)
   dates_converted <- as.Date(dates[[1]][1], format = "%d/%m/%Y")
   
+  #that one depend in the page that you want to extract but first you have to check if the file has the page you are trying to extract
  if (length(pdf_1) < 4) {
     warning(paste("Le fichier", fichier, "n'a pas assez de pages."))
     return(NULL)
   }
   
+  #extracting the specifiic page
   page_3 <- pdf_1[4]
   
-
+  #remove the arabic text in the extracted page
   page_3_data <- remove_arabic_text(page_3)
   
- 
+  #separate all lines to make it clearer
   lines <- strsplit(page_3_data, "\n")[[1]]
   
-
+  #seperates it column to have a look of the data as table form
   donnees <- str_split_fixed(lines, "\\s{2,}", n = 5)
   
+  #create the data frame
   df_chart <- as.data.frame(donnees, stringsAsFactors = FALSE)
   
   if (nrow(df_chart) == 0) {
@@ -47,12 +57,16 @@ extract_transform <- function(fichier) {
     return(NULL)
   }
   
+  #specify the column names
   colnames(df_chart) <- c("none", "Region", "Nouveaux_Cas", "Deces" , "date")
   
+  #delete the extra unuse colomn
   df_chart$none <- NULL
   
+  #fill the column date
   df_chart$date <- dates_converted 
   
+  #delete non numeric caracter on "nouveau cas" column
   df_chart$Nouveaux_Cas <- as.numeric(df_chart$Nouveaux_Cas)
   
   df_chart$Region <- sapply(df_chart$Region, remove_numeric_text)
@@ -82,7 +96,17 @@ df_final$Region <- gsub("région", "", df_final$Region, ignore.case = TRUE)
 
 View(df_final)
 
+local_path <- "/home/tsiory/Documents/cours_1è_année_ingenieur/stat_data/data/database/resultats_final_page4.csv"
+
 write.csv(df_final_3, "/home/tsiory/Documents/cours_1è_année_ingenieur/stat_data/data/database/resultats_final_page4.csv", row.names = FALSE)
+
+id_folder_destination <- "1LfRoDaciv5aXBRVkOQ8ZHMHuJ_782pby"
+
+drive_upload(
+  media = local_path,
+  path = as_id(id_folder_destination),
+  name = "ville_list1.csv"
+)
 
 
 
